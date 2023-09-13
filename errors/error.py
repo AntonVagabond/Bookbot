4e17e6fe11fba8db5a7502c5be8782ef9d0e12fd
Anton Akuev <antonakuev945@mail.ru>
2023-09-13 13:27:51 +0300

    Добавление папки и файла с исключением

---
+++ errors/error.py
@@ -0,0 +1,7 @@
+class ImproperlyConfigured(Exception):
+    """Raises when a environment variable is missing"""
+
+    def __init__(self, variable_name):
+        self.variable_name = variable_name
+        self.message = f'Set the {variable_name} environment variable.'
+        super().__init__(self.message)
