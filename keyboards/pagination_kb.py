769d40defd99e068bac5a8d9ceffcbf57cf4f64f
Anton Akuev <antonakuev945@mail.ru>
2023-09-15 09:48:37 +0300

    Отредачил функцию и поменял комментарии.

--- keyboards/pagination_kb.py
+++ keyboards/pagination_kb.py
@@ -3,13 +3,19 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 from lexicon.lexicon import LEXICON
 
 
-# Функция, генерирующая клавиатуру для страницы книги
+# A function that generates a keyboard for a book page
 def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
-    # Инициализируем билдер
+    # Initializing the builder
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
-    # Добавляем в билдер ряд с кнопками
-    kb_builder.row(*[InlineKeyboardButton(
-        text=LEXICON.get(button, button),
-        callback_data=button) for button in buttons])
-    # Возвращаем объект инлайн-клавиатуры
+
+    # Adding a row with buttons to the builder
+    kb_builder.row(
+        *[
+            InlineKeyboardButton(
+                text=LEXICON.get(button, button),
+                callback_data=button)
+            for button in buttons
+        ]
+    )
+    # Returning the inline keyboard object
     return kb_builder.as_markup()
