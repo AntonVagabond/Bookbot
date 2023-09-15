fde1a36b5b3d74746af6fedd1d34a11b92f80e0d
Anton Akuev <antonakuev945@mail.ru>
2023-09-15 06:19:03 +0300

    Закоментировал функции

--- keyboards/book_kb.py
+++ keyboards/book_kb.py
@@ -5,9 +5,12 @@ from callback_factories.edit_items import EditItemsCallbackFactory
 from lexicon.lexicon import LEXICON
 
 
+# Book list generation function
 def create_books_keyboard(*args: str) -> InlineKeyboardMarkup:
+    # Creating a keyboard object
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
 
+    # We fill the keyboard with buttons-books in ascending order
     for book_name in sorted(args):
         kb_builder.row(
             InlineKeyboardButton(
@@ -16,6 +19,7 @@ def create_books_keyboard(*args: str) -> InlineKeyboardMarkup:
             )
         )
 
+    # Add two "Edit" and "Cancel" buttons to the keyboard at the end
     kb_builder.row(
         InlineKeyboardButton(
             text=LEXICON['edit_button'],
@@ -27,13 +31,14 @@ def create_books_keyboard(*args: str) -> InlineKeyboardMarkup:
         ),
         width=2
     )
-
     return kb_builder.as_markup()
 
 
 def create_edit_books_keyboard(*args: str) -> InlineKeyboardMarkup:
+    # Creating a keyboard object
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
 
+    # We fill the keyboard with buttons-books in ascending order
     for book_name in sorted(args):
         if book_name == '📖 Ray Bradbury `The Martian Chronicles`':
             continue
@@ -45,11 +50,11 @@ def create_edit_books_keyboard(*args: str) -> InlineKeyboardMarkup:
             )
         )
 
+    # Add the "Cancel book editing" button to the end of the keyboard
     kb_builder.row(
         InlineKeyboardButton(
             text=LEXICON['cancel'],
             callback_data='cancel_edit_book'
         )
     )
-
     return kb_builder.as_markup()
