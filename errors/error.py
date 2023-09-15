f13d4e6ff59092b38757f89b2ce3ec873a4bc29c
Anton Akuev <antonakuev945@mail.ru>
2023-09-15 10:05:35 +0300

    Добавил класс, который вызывается, когда не находит файл.

--- errors/error.py
+++ errors/error.py
@@ -5,3 +5,11 @@ class ImproperlyConfigured(Exception):
         self.variable_name = variable_name
         self.message = f'Set the {variable_name} environment variable.'
         super().__init__(self.message)
+
+
+class BadBookError(Exception):
+    """Raises when it is not possible to parse this file"""
+
+    def __init__(self):
+        self.message = f"Can't parse this file"
+        super().__init__(self.message)
