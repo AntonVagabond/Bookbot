da2407a5fc0bc4017e347bf20708be1e0e557d05
Anton Akuev <antonakuev945@mail.ru>
2023-09-15 09:44:59 +0300

    Незначительная поправка

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -6,8 +6,7 @@ from lexicon.lexicon import LEXICON_COMMANDS
 
 # Функция для настройки кнопки Menu бота
 async def set_main_menu(bot: Bot):
-    main_menu_commands = [BotCommand(
-        command=command,
-        description=description
-    ) for command, description in LEXICON_COMMANDS.items()]
+    main_menu_commands = [
+        BotCommand(command=command, description=description)
+        for command, description in LEXICON_COMMANDS.items()]
     await bot.set_my_commands(main_menu_commands)
