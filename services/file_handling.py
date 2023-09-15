d989afec5da7ebcb3b7468dd27e78ba996c3267f
Anton Akuev <antonakuev945@mail.ru>
2023-09-15 11:36:59 +0300

    Добавил две последние функции, и доработал функции.

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,43 +1,64 @@
-import os
-import sys
+import json
 import re
-import itertools
 
-BOOK_PATH = 'book/book.txt'
+import requests
+import itertools as it
+
+from config_data.config import BOT_TOKEN
+from errors.error import BadBookError
+from typing import Any
+
 PAGE_SIZE = 1050
 
-book: dict[int, str] = {}
+
+# A function that returns a string with the page text and its size
+def _get_part_text(text: str, start: int, page_size: int) -> tuple[str, int] | None:
+    # we use HTML parse mode in our bot, so we need to edit some symbols
+
+    for old, new in zip(('<', '>', '&'), ('&lt', '&gt', '&amp')):
+        text: str = text.replace(old, new)
+    end: int = start + page_size
+    if end >= len(text):
+        return text[start:], len(text) - start
+
+    for i in range(end - 1, start - 1, -1):
+        if text[i] in ",.!:;?":
+            return text[start:i + 1], i + 1 - start
+
+    raise BadBookError()
 
 
-# Функция, возвращающая строку с текстом страницы и ее размер
-def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
-    filter_one: str = re.sub(r'[.,!?:;]\.+$', '', text[start:start + size])
-    filter_two: list[str] = re.findall(r'(?s).+[.,!?:;]', filter_one)
+# The function that forms the dictionary of the book
+def prepare_book(text: str) -> str:
+    content: dict = {}
 
-    # убирает лишние \n
-    filter_three: str = re.sub(r'\n(?! {4})', ' ', filter_two[0])
-    return filter_three, len(filter_three)
+    text_length: int = len(text)
 
-
-# Функция, формирующая словарь книги
-def prepare_book(path: str) -> None:
-    # Открываем файл
-    with open(path, 'r', encoding='utf-8') as file:
-        text: str = file.read()
-
-    # Количество символов в тексте
-    all_size: int = len(text)
-
-    # С какого места будет начинаться новая страница
-    start: int = 0
-    for page in itertools.count(start=1):
-        if all_size <= 0:
+    # Page Start
+    cur_idx: int = 0
+    for page in it.count(start=1):
+        if text_length <= 0:
             break
-        page_content, page_size = _get_part_text(text=text, start=start, size=PAGE_SIZE)
-        book[page] = page_content.lstrip()
-        all_size -= page_size
-        start += page_size
+        page_text, page_length = _get_part_text(text=text, start=cur_idx, page_size=PAGE_SIZE)
+        content[page] = page_text.lstrip()
+        text_length -= page_length
+        cur_idx += page_length
+
+    return json.dumps(content)
 
 
-# Вызов функции prepare_book для подготовки книги из текстового файла
-prepare_book(os.path.join(sys.path[0], os.path.normpath(BOOK_PATH)))
+def get_file_text_from_server(file_id: str) -> str:
+    file_info_url: str = f'https://api.telegram.org/bot{BOT_TOKEN}/getFile?file_id={file_id}'
+    response: requests = requests.get(file_info_url)
+    file_info: dict = response.json()
+    file_path: str = file_info['result']['file_path']
+    file_url: str = f'https://api.telegram.org/file/bot{BOT_TOKEN}/{file_path}'
+    response = requests.get(file_url)
+    return response.text
+
+
+def pretty_name(name: str) -> str:
+    name: str = name.replace('.txt', '')
+    pretty: str = re.sub(r'\W', '_', name.lower())
+    pretty: str = re.sub(r'_+', '_', pretty)
+    return pretty[:35]
